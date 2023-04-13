5e952c6be7ebf4a3c568041de2c1d87628ef4d15
quentinM22 <quentin.meunier1@laposte.net>
2023-04-13 14:16:24 +0200

    Feat: View Home

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,7 +1,32 @@
-import React from "react"
+import React, { useEffect } from "react"
+
+import Header from "../../components/Header/Header"
+import Banner from "../../components/Banner/Banner"
+import Card from "../../components/Card/Card"
+import Footer from "../../components/Footer/Footer"
+
+import Data from "../../data/logements.json"
 
 const Home = () => {
-	return <div>Home</div>
+	// Changement title onglet page
+	useEffect(() => {
+		document.title = `Kasa - Accueil`
+	})
+
+	return (
+		<div>
+			<div className="cmp-body">
+				<Header />
+				<Banner />
+				<div className="cmp-card-container">
+					{Data.map((item) => (
+						<Card key={item.id} title={item.title} />
+					))}
+				</div>
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
 export default Home
