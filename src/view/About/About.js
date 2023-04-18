ff64ef8c8da169cb6aa47e794672ceba48bf95d1
quentinM22 <quentin.meunier1@laposte.net>
2023-04-18 17:25:23 +0200

    Feat: Import > Fetch Data

--- src/view/About/About.js
+++ src/view/About/About.js
@@ -1,23 +1,33 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Header from "../../components/Header/Header"
 import Banner from "../../components/Banner/Banner"
 import Footer from "../../components/Footer/Footer"
 
-import Data from "../../data/about.json"
 import AboutCard from "../../components/AboutCard/AboutCard"
 
 const About = () => {
-	// Changement title onglet page
+	const [about, setAbout] = useState([])
+	const getData = async () => {
+		await fetch("data/about.json")
+			.then((res) => {
+				return res.json()
+			})
+			.then((data) => {
+				setAbout(data)
+			})
+			.catch((err) => console.error(err))
+	}
 	useEffect(() => {
 		document.title = `Kasa - A propos`
-	})
+		getData()
+	}, [])
 	return (
 		<>
 			<Header />
 			<div className="cmp-body">
 				<Banner />
 				<div className="cmp-about-card-container">
-					{Data.map((item, index) => (
+					{about.map((item, index) => (
 						<AboutCard
 							key={index}
 							title={item.title}
--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,25 +1,33 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 import Header from "../../components/Header/Header"
 import Banner from "../../components/Banner/Banner"
 import LocationCard from "../../components/LocationCard/LocationCard"
 import Footer from "../../components/Footer/Footer"
 
-import Data from "../../data/logements.json"
-
 const Home = () => {
-	// Changement title onglet page
+	const [logements, setLogements] = useState([])
+	const getData = async () => {
+		await fetch("data/logements.json")
+			.then((res) => {
+				return res.json()
+			})
+			.then((data) => {
+				setLogements(data)
+			})
+			.catch((err) => console.error(err))
+	}
 	useEffect(() => {
 		document.title = `Kasa - Accueil`
-	})
-
+		getData()
+	}, [])
 	return (
 		<>
 			<Header />
 			<div className="cmp-body">
 				<Banner />
 				<div className="cmp-card-container">
-					{Data.map((item) => (
+					{logements.map((item) => (
 						<LocationCard
 							key={item.id}
 							title={item.title}
